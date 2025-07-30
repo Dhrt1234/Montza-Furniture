9c8fc455d03fa4c21f2e894d2a8254e86a0444fc
Dharti Patel <167915646+Dhrt1234@users.noreply.github.com>
2025-07-30 22:51:14 +0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,13 @@ const { webRoutes } = require("./App/routes/web/webRoutes");
 const { companyModel } = require("./App/models/companyModel");
 // app.use(cors())
 app.use(cors({
-  origin: 'https://montza-furniture-admin.vercel.app',
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
